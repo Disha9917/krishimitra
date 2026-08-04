5431e5e6b869f0cf867c7ab72b35e8dc0d0f6549
Shlok Solanki <shlokpsolanki2020@gmail.com>
2026-08-04 15:07:20 +0530

    landing page updated

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,13 +3,8 @@ import { Navbar } from "../components/layout/navbar";
 import { Hero } from "../components/landing/hero";
 import { Features } from "../components/landing/features";
 import { HowItWorks } from "../components/landing/how-it-works";
-import { Statistics } from "../components/landing/statistics";
-import { Benefits } from "../components/landing/benefits";
-import { Testimonials } from "../components/landing/testimonials";
-import { FAQs } from "../components/landing/faqs";
 import { AboutSection } from "../components/landing/about-section";
 import { ContactSection } from "../components/landing/contact-section";
-import { CTA } from "../components/landing/cta";
 import { Footer } from "../components/layout/footer";
 import { LiveBreezeBackground } from "../components/landing/live-breeze-background";
 
@@ -24,13 +19,8 @@ export default function LandingPage() {
         <Hero />
         <Features />
         <HowItWorks />
-        <Statistics />
-        <Benefits />
         <AboutSection />
-        <Testimonials />
-        <FAQs />
         <ContactSection />
-        <CTA />
       </main>
       <Footer />
     </div>
